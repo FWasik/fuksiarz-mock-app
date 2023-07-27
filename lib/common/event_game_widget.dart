e8bdf7c8f353d06c8d38eceaf2bac36f4db0e8e0
FWasik <franekwasik2@gmail.com>
2023-07-27 20:49:22 +0200

    updating event_game_widget for live events on searching

--- lib/common/event_game_widget.dart
+++ lib/common/event_game_widget.dart
@@ -25,6 +25,8 @@ class EventGameWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
+    bool isLiveAndSearched =
+        event is EventSearched && (event as EventSearched).area == "LIVE_EVENT";
 
     return LayoutBuilder(builder: (context, constraints) {
       return Padding(
@@ -35,87 +37,17 @@ class EventGameWidget extends StatelessWidget {
             constraints: BoxConstraints(minWidth: constraints.maxWidth),
             padding: const EdgeInsets.all(10.0),
             decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey[300]!),
-                borderRadius: BorderRadius.circular(10.0)),
+              border: Border.all(color: Colors.grey[300]!),
+              borderRadius: BorderRadius.circular(10.0),
+            ),
             child: IntrinsicWidth(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Row(
-                    children: [
-                      Text(
-                        "${subsubcategory.name}   ${event.eventDate}",
-                        style: const TextStyle(
-                            fontSize: 12, fontWeight: FontWeight.bold),
-                      ),
-                      const Spacer(flex: 30),
-                      if (event is EventSearched) ...[
-                        buildIconContainer(
-                          "TV",
-                          Colors.blueGrey[300]!,
-                          const Icon(
-                            Icons.tv,
-                            color: Colors.white,
-                            size: 14.0,
-                          ),
-                          45,
-                        ),
-                        const Spacer(flex: 1),
-                      ],
-                      buildIconContainer(
-                        "HOT",
-                        Colors.red,
-                        const Icon(
-                          Icons.local_fire_department_outlined,
-                          color: Colors.white,
-                          size: 14.0,
-                        ),
-                        50,
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 15),
-                  Row(
-                    children: [
-                      Text(
-                        event.eventTime,
-                        style: const TextStyle(
-                            fontSize: 12, fontWeight: FontWeight.bold),
-                      ),
-                      const Flexible(
-                        child: Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 8.0),
-                          child: LinearProgressIndicator(
-                            minHeight: 0.8,
-                            value: 0,
-                            backgroundColor: Colors.grey,
-                            valueColor:
-                                AlwaysStoppedAnimation<Color>(Colors.red),
-                          ),
-                        ),
-                      ),
-                      const Text(
-                        "+ 111",
-                        style: TextStyle(
-                            fontSize: 12,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.red),
-                      )
-                    ],
-                  ),
-                  const SizedBox(height: 15),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      buildGameName(width),
-                      buildOdds(),
-                    ],
-                  ),
-                  if (event is EventSearched &&
-                      (event as EventSearched).area == "LIVE_EVENT") ...[
-                    const SizedBox(height: 15),
-                    buildAdditionalInfo()
-                  ]
+                  buildFirstRow(),
+                  buildSecondRow(isLiveAndSearched, width),
+                  buildThirdRow(isLiveAndSearched, width),
+                  if (isLiveAndSearched) ...[buildAdditionalInfo()]
                 ],
               ),
             ),
@@ -125,13 +57,204 @@ class EventGameWidget extends StatelessWidget {
     });
   }
 
+  Widget buildFirstRow() {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(right: 30.0),
+            child: Text(
+              "${subsubcategory.name}   ${event.eventDate}",
+              style: const TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                if (event is EventSearched) ...[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                    child: buildIconContainer(
+                      "TV",
+                      Colors.blueGrey[300]!,
+                      const Icon(
+                        Icons.tv,
+                        color: Colors.white,
+                        size: 14.0,
+                      ),
+                      45,
+                    ),
+                  ),
+                ],
+                Padding(
+                  padding: const EdgeInsets.only(left: 4.0),
+                  child: buildIconContainer(
+                    "HOT",
+                    Colors.redAccent[700]!,
+                    const Icon(
+                      Icons.local_fire_department_outlined,
+                      color: Colors.white,
+                      size: 14.0,
+                    ),
+                    50,
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget buildSecondRow(bool isLiveAndSearched, double width) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        children: [
+          isLiveAndSearched
+              ? buildIconContainer(
+                  "60'",
+                  Colors.green,
+                  const Icon(
+                    Icons.circle,
+                    color: Colors.white,
+                    size: 7,
+                  ),
+                  40)
+              : Text(
+                  event.eventTime,
+                  style: const TextStyle(
+                      fontSize: 12, fontWeight: FontWeight.bold),
+                ),
+          Flexible(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: buildIndicator(isLiveAndSearched, width),
+            ),
+          ),
+          Text(
+            "+ 111",
+            style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+                color: Colors.redAccent[700]),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget buildThirdRow(bool isLiveAndSearched, double width) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          buildGameName(width),
+          if (isLiveAndSearched) ...[
+            const Spacer(flex: 10),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "1",
+                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 6.0),
+                  Text(
+                    "0",
+                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "1",
+                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 6.0),
+                  Text(
+                    "0",
+                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "1",
+                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 6.0),
+                  Text(
+                    "0",
+                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "1",
+                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 6.0),
+                  Text(
+                    "0",
+                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "1",
+                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 6.0),
+                  Text(
+                    "0",
+                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ),
+          ],
+          buildOdds(),
+        ],
+      ),
+    );
+  }
+
   Widget buildGameName(double width) {
     List<String> teamSplitted = event.eventName.split(" - ");
 
     if ((eventGame != null && eventGame!.gameLayout == 9) ||
         teamSplitted.length == 1) {
       return SizedBox(
-        width: width * 0.4,
+        width: width * 0.45,
         child: Text(
           event.eventName,
           maxLines: 4,
@@ -140,7 +263,7 @@ class EventGameWidget extends StatelessWidget {
       );
     } else {
       return SizedBox(
-        width: width * 0.4,
+        width: width * 0.40,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -211,7 +334,11 @@ class EventGameWidget extends StatelessWidget {
           icon,
           Text(
             text,
-            style: const TextStyle(color: Colors.white, fontSize: 10),
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 10,
+              fontWeight: FontWeight.bold,
+            ),
           )
         ]),
       ),
@@ -219,34 +346,71 @@ class EventGameWidget extends StatelessWidget {
   }
 
   Widget buildAdditionalInfo() {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.grey[200],
-        borderRadius: BorderRadius.circular(30),
-      ),
-      child: Row(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(5.0),
-            child: Container(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 8.0, vertical: 3.0),
-              decoration: BoxDecoration(
-                color: Colors.grey[500],
-                borderRadius: BorderRadius.circular(30),
-              ),
-              child: Text(
-                event.eventTime,
-                style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                    fontSize: 10),
+    return Padding(
+      padding: const EdgeInsets.only(top: 16.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey[200],
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 3.0),
+                decoration: BoxDecoration(
+                  color: Colors.grey[500],
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Text(
+                  event.eventTime,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 10),
+                ),
               ),
             ),
-          ),
-          const Text("Coś o czymś...", style: TextStyle(fontSize: 12))
-        ],
+            const Text("Coś o czymś...", style: TextStyle(fontSize: 12))
+          ],
+        ),
       ),
     );
+  }
+
+  Widget buildIndicator(bool isLiveAndSearched, double width) {
+    double currentValue = 60;
+
+    if (isLiveAndSearched) {
+      return Row(
+        children: [
+          Flexible(
+            child: LinearProgressIndicator(
+              minHeight: 3,
+              value: currentValue / 45,
+              backgroundColor: Colors.grey,
+              valueColor: const AlwaysStoppedAnimation<Color>(Colors.green),
+            ),
+          ),
+          const SizedBox(width: 10),
+          Flexible(
+            child: LinearProgressIndicator(
+              minHeight: 3,
+              value: (currentValue - 45) / 45,
+              backgroundColor: Colors.grey[300],
+              valueColor: const AlwaysStoppedAnimation<Color>(Colors.green),
+            ),
+          ),
+        ],
+      );
+    } else {
+      return LinearProgressIndicator(
+        minHeight: 2,
+        value: 0,
+        backgroundColor: Colors.grey[300],
+      );
+    }
   }
 }
